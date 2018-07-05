e4944874ba96c20f3d54735208581087d30d2acf
ShaySherlock <lykx98055_1@163.com>
2018-07-05 19:56:42 +0800

    第一次提交项目

---
+++ app/src/main/java/activity/text/com/text1/MainActivity.java
@@ -0,0 +1,16 @@
+package activity.text.com.text1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
